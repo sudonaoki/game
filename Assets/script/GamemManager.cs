a2d9f1977c1e7dc650f88d75bb6c6851a66a2e9e
sudonaoki <s2221118@cco.kanagawa-it.ac.jp>
2024-06-09 00:33:45 +0900

    first

---
+++ Assets/script/GamemManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamemManager : MonoBehaviour
+{
+   public void StartButton()
+    {
+        SceneManager.LoadScene("controll");
+    }
+
+    public void Quit()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
