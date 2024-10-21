628f4a3c43d5d4ef86c628ddf4c7900d5144348b
sudonaoki <s2221118@cco.kanagawa-it.ac.jp>
2024-10-21 19:37:29 +0900

    eigth

--- Assets/script/Game.cs
+++ Assets/script/Game.cs
@@ -39,16 +39,30 @@ public class Game : MonoBehaviour
     public InputField keypad;
     public GameObject check_button;
 
-    public GameObject off_panel1;
-    public GameObject off_panel2;
-    public GameObject off_panel3;
-    public GameObject off_panel4;
+
     public GameObject off_hint;
 
-    public GameObject itemgetpanel; // 表示するための全画面の透明な土台
-    public GameObject itemget;
+    public GameObject key1_getpanel;
+    public GameObject potato_getpanel;
+    public GameObject potato_eat_getpanel;
+    public GameObject key2_getpanel;
+    public GameObject baseball_getpanel;
+    public GameObject food_getpanel;
+    public GameObject controller_getpanel;
+    public GameObject CD_getpanel;
+    public GameObject radio_getpanel;
 
-    public GameObject key1_get; // 鍵入手時表示する
+    public GameObject key1_itemget;
+    public GameObject potato_itemget;
+    public GameObject eat_potato_itemget;
+    public GameObject key2_itemget;
+    public GameObject baseball_itemget;
+    public GameObject food_itemget;
+    public GameObject controller_itemget;
+    public GameObject CD_itemget;
+    public GameObject radio_itemget;
+
+    public GameObject key1_get;
     public GameObject potato_get;
     public GameObject eat_potato_get;
     public GameObject key2_get;
@@ -67,7 +81,6 @@ public class Game : MonoBehaviour
 
     void Start()
     {
-        itemgetpanel.SetActive(false); // デフォルトで非アクティブに設定
         open_door.SetActive(false);
         open_safe.SetActive(false);
         open_bookshelf.SetActive(false);
@@ -83,10 +96,26 @@ public class Game : MonoBehaviour
         escapepanel.SetActive(false);
 
         talkPanel.SetActive(false);
-        off_panel1.SetActive(false);
-        off_panel2.SetActive(false);
-        off_panel3.SetActive(false);
-        off_panel4.SetActive(false);
+
+        key1_getpanel.SetActive(false);
+        potato_getpanel.SetActive(false);   
+        potato_eat_getpanel.SetActive(false);   
+        key2_getpanel.SetActive(false);
+        baseball_getpanel.SetActive(false);
+        food_getpanel.SetActive(false);
+        controller_getpanel.SetActive(false);
+        CD_getpanel.SetActive(false);
+        radio_getpanel.SetActive(false);
+
+        key1_itemget.SetActive(false);
+        potato_itemget.SetActive(false);
+        eat_potato_itemget.SetActive(false);
+        key2_itemget.SetActive(false);
+        baseball_itemget.SetActive(false);
+        food_itemget.SetActive(false); 
+        controller_itemget.SetActive(false);
+        CD_itemget.SetActive(false);
+        radio_itemget.SetActive(false);
 
         key1_get.SetActive(false);
         potato_get.SetActive(false);
@@ -165,7 +194,10 @@ public class Game : MonoBehaviour
         check_button.SetActive(true);
     }
 
-
+    public void keypadtap()
+    {
+        keypadpanel.SetActive(true);
+    }
 
     public void cagetap()
     {
@@ -177,13 +209,24 @@ public class Game : MonoBehaviour
         }
     }
 
+    public void TVtap()
+    {
+        if (off_TV.activeInHierarchy)
+        {
+            off_TV.SetActive(false);
+            on_TV.SetActive(true);
+        }
+    }
 
 
+    //
     public void key1get()
     {
-        itemgetpanel.SetActive(true);
-        itemget.SetActive(true);
+        key1_getpanel.SetActive(true);
+        key1_itemget.SetActive(true);
+
         key1_get.SetActive(true);
+
         key1.SetActive(false);
     }
 
@@ -191,17 +234,19 @@ public class Game : MonoBehaviour
     {
         if (key1_get.activeInHierarchy == true)
         {
-            itemgetpanel.SetActive(false);
-            itemget.SetActive(false);
+            key1_getpanel.SetActive(false);
+            key1_itemget.SetActive(false);
             key1_get.SetActive(false);
         }
     }
 
     public void potatoget()
     {
-        itemgetpanel.SetActive(true);
-        itemget.SetActive(true);
+        potato_getpanel.SetActive(true);
+        potato_itemget.SetActive(true);
+
         potato_get.SetActive(true);
+
         potato.SetActive(false);
     }
 
@@ -209,17 +254,21 @@ public class Game : MonoBehaviour
     {
         if (potato_get.activeInHierarchy == true)
         {
-            itemgetpanel.SetActive(false);
-            itemget.SetActive(false);
+            potato_getpanel.SetActive(false);
+            potato_itemget.SetActive(false);
             potato_get.SetActive(false);
+            potato_eat_getpanel.SetActive(true);
+            eat_potato_itemget.SetActive(true);
+            eat_potato_get.SetActive(true);
         }
       
     }
 
     public void eat_potatoget()
     {
-        itemgetpanel.SetActive(true);
-        itemget.SetActive(true);
+        potato_eat_getpanel.SetActive(true);
+        eat_potato_itemget.SetActive(true);
+
         eat_potato_get.SetActive(true);
     }
 
@@ -227,34 +276,41 @@ public class Game : MonoBehaviour
     {
         if (eat_potato_get.activeInHierarchy == true)
         {
-            itemgetpanel.SetActive(false);
-            itemget.SetActive(false);
+            potato_eat_getpanel.SetActive(false);
+            eat_potato_itemget.SetActive(false);
             eat_potato_get.SetActive(false);
+            key2_getpanel.SetActive(true);
+            key2_itemget.SetActive(true);
+            key2_get.SetActive(true);
         }
     }
 
     public void key2get()
     {
-        itemgetpanel.SetActive(true);
-        itemget.SetActive(true);
+        key2_getpanel.SetActive(true);
+        key2_itemget.SetActive(true);
+
         key2_get.SetActive(true);
+
     }
 
     public void key2gettap()
     {
         if (key2_get.activeInHierarchy == true)
         {
-            itemgetpanel.SetActive(false);
-            itemget.SetActive(false);
+            key2_getpanel.SetActive(false);
+            key2_itemget.SetActive(false);
             key2_get.SetActive(false);
         }
     }
 
     public void baseballget()
     {
-        itemgetpanel.SetActive(true);
-        itemget.SetActive(true);
+        baseball_getpanel.SetActive(true);
+        baseball_itemget.SetActive(true);
+
         baseball_get.SetActive(true);
+
         baseball.SetActive(false);
         close_window.SetActive(false);
         Chime.SetActive(false);
@@ -266,17 +322,20 @@ public class Game : MonoBehaviour
     {
         if (baseball_get.activeInHierarchy == true)
         {
-            itemgetpanel.SetActive(false);
-            itemget.SetActive(false);
+            baseball_getpanel.SetActive(false);
+            baseball_itemget.SetActive(false);
             baseball_get.SetActive(false);
         }
     }
 
     public void foodget()
     {
-        itemgetpanel.SetActive(true);
-        itemget.SetActive(true);
+        food_getpanel.SetActive(true);
+        food_itemget.SetActive(true);
+
         food_get.SetActive(true);
+
+
         food1.SetActive(false);
     }
 
@@ -284,17 +343,19 @@ public class Game : MonoBehaviour
     {
         if (food_get.activeInHierarchy == true)
         {
-            itemgetpanel.SetActive(false);
-            itemget.SetActive(false);
+            food_getpanel.SetActive(false);
+            food_itemget.SetActive(false);
             food_get.SetActive(false);
         }
     }
 
     public void controllerget()
     {
-        itemgetpanel.SetActive(true);
-        itemget.SetActive(true);
+        controller_getpanel.SetActive(true);
+        controller_itemget.SetActive(true);
+
         controller_get.SetActive(true);
+
         controller.SetActive(false);
     }
 
@@ -302,9 +363,50 @@ public class Game : MonoBehaviour
     {
         if (controller_get.activeInHierarchy == true)
         {
-            itemgetpanel.SetActive(false);
-            itemget.SetActive(false);
+            controller_getpanel.SetActive(false);
+            controller_itemget.SetActive(false);
             controller_get.SetActive(false);
+            wall1.SetActive(false);
+            wall2.SetActive(true);
+            wall3.SetActive(false);
+            wall4.SetActive(false);
+        }
+    }
+
+    public void CDget()
+    {
+        CD_getpanel.SetActive(true);
+        CD_itemget.SetActive(true);
+
+        CD_get.SetActive(true);
+    }
+
+    public void CDgettap()
+    {
+        if (CD_get.activeInHierarchy == true)
+        {
+            CD_getpanel.SetActive(false);
+            CD_itemget.SetActive(false);
+            CD_get.SetActive(false);
+        }
+    }
+    public void radioget()
+    {
+        radio_getpanel.SetActive(true);
+        radio_itemget.SetActive(true);
+
+        radio_get.SetActive(true);
+
+
+    }
+
+    public void radiogettap()
+    {
+        if (radio_get.activeInHierarchy == true)
+        {
+            radio_getpanel.SetActive(false);
+            radio_itemget.SetActive(false);
+            radio_get.SetActive(false);
         }
     }
 }
--- Assets/script/KeyPadManager.cs
+++ Assets/script/KeyPadManager.cs
@@ -7,7 +7,6 @@ public class PassManager : MonoBehaviour
 {
 
     public InputField passwordField;
-    public Text resultText;
     public GameObject close_refrigerator; 
     public GameObject open_refrigerator;  
 
@@ -25,11 +24,7 @@ public class PassManager : MonoBehaviour
             close_refrigerator.SetActive(false);
             open_refrigerator.SetActive(true);
         }
-        else
-        {
-            resultText.text = "Password is incorrect!";
-            resultText.color = Color.red;
-        }
+        
     }
 }
 
